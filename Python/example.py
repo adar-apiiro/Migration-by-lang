318d21c6abdf9161b2359855b68c7a5d42f5c32b
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-08 15:22:33 +0300

    Create example.py

---
+++ Python/example.py
@@ -0,0 +1,11 @@
+import collections  # From Python standard library.
+import bson
+from bson.codec_options import CodecOptions
+
+data = bson.BSON.encode({'a': 1})
+decoded_doc = bson.BSON(data).decode()
+print(type(decoded_doc))  # Output: <class 'dict'>
+
+options = CodecOptions(document_class=collections.OrderedDict)
+decoded_doc = bson.BSON(data).decode(codec_options=options)
+print(type(decoded_doc))  # Output: <class 'collections.OrderedDict'>
