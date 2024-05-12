d5dfa9f38431c320eb03a29bece8d8f02ea09884
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-12 13:15:25 +0300

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -4,6 +4,8 @@ from bson.codec_options import CodecOptions
 import aws-cli 
 import chacha20
 import boto
+import boto3
+import botocore
 
 data = bson.BSON.encode({'a': 1})
 decoded_doc = bson.BSON(data).decode()
