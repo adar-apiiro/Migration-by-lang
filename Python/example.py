a2b25822311674aae23564f4a9b147a2039167b4
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-12 10:51:48 +0300

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -4,8 +4,9 @@ from bson.codec_options import CodecOptions
 
 data = bson.BSON.encode({'a': 1})
 decoded_doc = bson.BSON(data).decode()
-print(type(decoded_doc))  # Output: <class 'dict'>
+print(type(decoded_doc)) 
+
 
 options = CodecOptions(document_class=collections.OrderedDict)
 decoded_doc = bson.BSON(data).decode(codec_options=options)
-print(type(decoded_doc))  # Output: <class 'collections.OrderedDict'>
+print(type(decoded_doc)) 
