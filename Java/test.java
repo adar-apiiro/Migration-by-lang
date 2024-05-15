2e7c437f1a1dcb7d90273f27678fcf8e007f6ad6
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-15 09:56:08 +0300

    Create test.java

---
+++ Java/test.java
@@ -0,0 +1,22 @@
+import io.dropwizard
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // here you can initialize tasks, bundles, etc.
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource();
+        environment.jersey().register(resource);
+    }
+}
